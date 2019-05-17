a9d5cca39d2aa5d198bc12ba21b06d6bd91ef46a
s3660969 <ec2-user@ip-172-31-88-11.ec2.internal>
2019-05-17 09:16:39 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,15 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+    config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '97d47bbaa2554fc4b803a867594f8c83.vfs.cloud9.us-east-1.amazonaws.com' 
+  # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   config.cache_classes = false
 
   # Do not eager load code on boot.
