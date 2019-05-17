8c87b8abf2796dfc6f95b7128c794650405c62f9
s3660969 <ec2-user@ip-172-31-88-11.ec2.internal>
2019-05-17 10:55:20 +0000

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,14 +7,13 @@ module SessionsHelper
   
     # Forgets a persistent session.
   def forget(user)
-    user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
 
   # Logs out the current user.
   def log_out
-    forget(current_user)
+    current_user && forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -42,7 +41,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
